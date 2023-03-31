14147e37c3b75b42592d944ba4e416d19e4e2d5f
yevhenkr <vasilenkoevgen88@gmail.com>
2023-03-31 11:08:01 +0300

    Added mixit up in js whithout plugin

--- app/js/main.js
+++ app/js/main.js
@@ -50,8 +50,39 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // document.addEventListener("DOMContentLoaded", () => {
+  const filterBtns = document.querySelectorAll(".categories-nav__btn");
+  const grid = document.querySelector(".categories-list");
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // remove active class from all buttons
+      filterBtns.forEach((filterBtn) => {
+        filterBtn.classList.remove("active");
+      });
+      // add active class to clicked button
+      btn.classList.add("active");
 
+      // get the filter value from the clicked button
+      const filterValue = btn.getAttribute("data-filter");
+
+      // filter the grid items based on the filter value
+      for (const item of grid.children) {
+        if (filterValue === "all") {
+          // item.style.display = "block";
+          item.classList.remove('hide');
+          item.classList.add('show');
+        } else if (item.classList.contains(filterValue)) {
+          // item.style.display = "block";
+          item.classList.remove('hide')
+          item.classList.add('show')
+        } else {
+          // item.style.display = "none";
+          item.classList.remove('show')
+          item.classList.add('hide')
+        }
+      }
+    });
+  });
+  
   const header = document.querySelector('.header');
   window.onscroll = () => {
     if (window.pageYOffset > 100) {
